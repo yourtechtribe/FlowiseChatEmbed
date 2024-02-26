da51a5f7b8a222a039d048f5062e0ba34ced7764
Your Tech Tribe <149100419+yourtechtribe@users.noreply.github.com>
2024-02-26 07:21:10 +0100

    Update Badge.tsx

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -50,17 +50,17 @@ export const Badge = (props: Props) => {
         'background-color': props.badgeBackgroundColor ?? '#ffffff',
       }}
     >
-      Powered by
+      Made with ❤ by
       <a
         ref={liteBadge}
-        href={'https://flowiseai.com'}
+        href={'https://www.uab.cat/enginyeria/'}
         target="_blank"
         rel="noopener noreferrer"
         class="lite-badge"
         id="lite-badge"
         style={{ 'font-weight': 'bold', color: props.poweredByTextColor ?? defaultTextColor }}
       >
-        <span> Flowise</span>
+        <span> #EnginyeriaUAB</span>
       </a>
     </span>
   );
